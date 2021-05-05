8b56bfb25324375770f267454733c1ca5d6483c8
pareshrane1998 <pareshr32@gmail.com>
2021-05-05 19:27:25 +0530

    initial js addded

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,40 @@
 /* Author: 
 
 */
+var dropTarget = document.querySelector(".wrapper");
+var draggables = document.querySelectorAll(".tasks");
+
+// document.getElementById("start").ondragstart = function(){ return false;}
+
+for(let i = 0; i < draggables.length; i++) {
+  draggables[i].addEventListener("dragstart", function (ev) {
+     ev.dataTransfer.setData("srcId", ev.target.id);
+  });
+}
+
+dropTarget.addEventListener('dragover', function(ev) {
+  ev.preventDefault();
+});
+
+dropTarget.addEventListener('drop', function(ev) {
+  ev.preventDefault();
+  let target = ev.target;
+  let droppable  = target.classList.contains('container2');
+  let srcId = ev.dataTransfer.getData("srcId");
+  
+  if (droppable) {
+    ev.target.appendChild(document.getElementById(srcId));
+  }
+});
+
+
+
+
+
+
+
+
+
 
 
 
